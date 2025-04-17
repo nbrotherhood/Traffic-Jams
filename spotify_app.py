eda8249d790d28db1a807f1ae7f7b2c1dfd2a37c
nbrotherhood <nbrother30@gmail.com>
2025-04-16 20:18:18 -0400

    Update spotify_app.py
    Updated Line 14 from:
    'st.set_page_config(page_title="Spotify Stats")'
    To:
    'st.set_page_config(page_title="Your Spotify Stats")'

--- spotify_app.py
+++ spotify_app.py
@@ -11,7 +11,7 @@ import uuid
 import requests
 import json
 
-st.set_page_config(page_title="Spotify Stats")
+st.set_page_config(page_title="Your Spotify Stats")
 
 CLIENT_ID = "ccd5da2397674bcaa675148a646996c3"
 REDIRECT_URI = "http://127.0.0.1:3000"
